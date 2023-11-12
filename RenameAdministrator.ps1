9e8aa7eb17af072c39858e922249a09cd7d9699b
Roger <110936182+rogerbase@users.noreply.github.com>
2023-11-12 21:25:05 +0100

    Update RenameAdministrator.ps1

--- RenameAdministrator.ps1
+++ RenameAdministrator.ps1
@@ -1,43 +1,78 @@
 # Display header
-Write-Host "-------------------------------------------------"
-Write-Host "- $($MyInvocation.MyCommand.Name)"
-Write-Host "- "
-Write-Host "- This script allows you to rename the Administrator account."
-Write-Host "- "
-Write-Host "-------------------------------------------------"
+function Display-Header {
+    Write-Host "-------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "- $($MyInvocation.MyCommand.Name)" -ForegroundColor Yellow
+    Write-Host "- This script allows you to rename the Administrator account." -ForegroundColor Cyan
+    Write-Host "-------------------------------------------------" -ForegroundColor Cyan
+}
 
-# Check for admin rights
-$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+function Check-AdminRights {
+    $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    
+    if (-not $isAdmin) {
+        Write-Host "ERROR: You lack the necessary permissions to rename the Administrator account." -ForegroundColor Red
+        Write-Host "Please right-click and run as administrator." -ForegroundColor Red
+        Read-Host "Press Enter to exit..."
+        exit
+    } else {
+        Write-Host "[Admin rights confirmed]" -ForegroundColor Green
+    }
+}
 
-if (-not $isAdmin) {
-    Write-Host "ERROR: You lack the necessary permissions to rename the Administrator account. Please right-click and run as administrator." -ForegroundColor Red
+function Get-CurrentAdminUsername {
+    return (Get-WmiObject -Class Win32_UserAccount -Filter "SID like 'S-1-5-%-500'").Name
+}
+
+function Get-NewUsername {
+    param (
+        [string]$current_name
+    )
+
+    $new_name = Read-Host "Enter the new username (Press Enter to keep '$current_name')"
+    if (-not $new_name) { $new_name = $current_name }
+    return $new_name
+}
+
+function Rename-AdministratorAccount {
+    param (
+        [string]$new_name
+    )
+
+    $adminAccount = Get-WmiObject -Class Win32_UserAccount -Filter "SID like 'S-1-5-%-500'"
+    $adminAccount.Rename($new_name) | Out-Null
+}
+
+function Check-RenamingOperation {
+    param (
+        [string]$new_name
+    )
+
+    $current_name = Get-CurrentAdminUsername
+
+    if ($current_name -ne $new_name) {
+        Write-Host "ERROR: Failed to rename the Administrator account." -ForegroundColor Red
+        Read-Host "Press Enter to exit..."
+        exit
+    } else {
+        Write-Host "The Administrator account has been renamed to '$new_name'" -ForegroundColor Green
+    }
+}
+
+# Main script execution
+Display-Header
+Check-AdminRights
+$current_name = Get-CurrentAdminUsername
+Write-Host "Current Administrator account name: '$current_name'" -ForegroundColor Yellow
+
+$new_name = Get-NewUsername -current_name $current_name
+if ($new_name -eq $current_name) {
+    Write-Host "No change in account name. Exiting script." -ForegroundColor Cyan
     Read-Host "Press Enter to exit..."
     exit
-} else {
-    Write-Host "[Admin rights confirmed]"
 }
 
-# Get the new name
-$new_name = Read-Host "Enter the new username (Press Enter for default 'Administrator')"
-if (-not $new_name) { $new_name = "Administrator" }
+Rename-AdministratorAccount -new_name $new_name
+Check-RenamingOperation -new_name $new_name
 
-Write-Host "- The Administrator account will be renamed to '$new_name'"
-Read-Host "Press Enter to continue..."
-
-# Rename the Administrator account
-Get-WmiObject -Class Win32_UserAccount -Filter "SID like 'S-1-5-%-500'" | ForEach-Object {
-    $_.Rename($new_name)
-}
-
-# Check the renaming operation
-$current_name = (Get-WmiObject -Class Win32_UserAccount -Filter "SID like 'S-1-5-%-500'").Name
-
-if ($current_name -ne $new_name) {
-    Write-Host "ERROR: Failed to rename the Administrator account." -ForegroundColor Red
-    Read-Host "Press Enter to exit..."
-    exit
-}
-
-# Done
-Write-Host "---------- Done"
+Write-Host "---------- Done ----------" -ForegroundColor Cyan
 Read-Host "Press Enter to exit..."
