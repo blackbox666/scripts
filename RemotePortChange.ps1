2cab1a9508f014f653c866ea6c1838bc903584d2
Roger <110936182+rogerbase@users.noreply.github.com>
2023-11-12 21:25:57 +0100

    Update RemotePortChange.ps1

--- RemotePortChange.ps1
+++ RemotePortChange.ps1
@@ -1,27 +1,57 @@
 # Display header
-Write-Host "-------------------------------------------------"
-Write-Host "- $($MyInvocation.MyCommand.Name)"
-Write-Host "- "
-Write-Host "- This script allows you to change the RDP port."
-Write-Host "- Note: The default RDP port is 3389 (0xd3d in hex)."
-Write-Host "- "
-Write-Host "- Here's the current port (in hex):"
-Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "PortNumber"
-Write-Host "-------------------------------------------------"
-
-# Check for admin rights
-$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-
-if (-not $isAdmin) {
-    Write-Host "ERROR: You lack the necessary permissions to change the RDP port. Please right-click and run as administrator." -ForegroundColor Red
-    Read-Host "Press Enter to exit..."
-    exit
-}
-else {
-    Write-Host "[Admin rights confirmed]"
+function Display-Header {
+    Write-Host "-------------------------------------------------" -ForegroundColor Cyan
+    Write-Host "- This script allows you to change the RDP port." -ForegroundColor Cyan
+    Write-Host "- Note: The default RDP port is 3389 (0xd3d in hex)." -ForegroundColor Cyan
+    Write-Host "-------------------------------------------------" -ForegroundColor Cyan
 }
 
-# Input port
+function Check-AdminRights {
+    $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    
+    if (-not $isAdmin) {
+        Write-Host "ERROR: You lack the necessary permissions to change the RDP port. Please right-click and run as administrator." -ForegroundColor Red
+        Read-Host "Press Enter to exit..."
+        exit
+    } else {
+        Write-Host "[Admin rights confirmed]" -ForegroundColor Green
+    }
+}
+
+function Get-CurrentRdpPort {
+    $currentPort = Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "PortNumber"
+    Write-Host "- Current RDP port (in hex):" $currentPort.PortNumber.ToString("X") -ForegroundColor Yellow
+}
+
+function Set-NewRdpPort {
+    param (
+        [int]$newPort
+    )
+
+    Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "PortNumber" -Value $newPort
+    Write-Host "-- RDP port updated to $newPort" -ForegroundColor Green
+}
+
+function Add-FirewallRule {
+    param (
+        [int]$port
+    )
+
+    Write-Host "-- Adding firewall rule for port $port..." -ForegroundColor Cyan
+    netsh advfirewall firewall add rule name="RDP Port $port" profile=any protocol=TCP action=allow dir=in localport=$port | Out-Null
+}
+
+function Restart-TerminalServices {
+    Write-Host "-- Restarting Terminal Services..." -ForegroundColor Cyan
+    Stop-Service -Name TermService -Force
+    Start-Service -Name TermService
+}
+
+# Main script execution
+Display-Header
+Check-AdminRights
+Get-CurrentRdpPort
+
 do {
     $rdp_port = Read-Host "Enter the desired port (Press Enter for the default 3389)"
     if (-not $rdp_port) { $rdp_port = 3389 }
@@ -31,27 +61,15 @@ do {
     }
 } until ($rdp_port -ge 1 -and $rdp_port -le 65535)
 
-Write-Host "- Proceeding will set the RDP port to $rdp_port"
+Read-Host "Press Enter to change RDP port to $rdp_port..."
+
+Set-NewRdpPort -newPort $rdp_port
+Get-CurrentRdpPort
+Write-Host "---------- Adding firewall rules and restarting services..." -ForegroundColor Cyan
 Read-Host "Press Enter to continue..."
 
-# Update the RDP port
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "PortNumber" -Value $rdp_port
+Add-FirewallRule -port $rdp_port
+Restart-TerminalServices
 
-# Confirm the port update
-Write-Host "- Here's the new port (in hex):"
-Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name "PortNumber"
-
-Write-Host "---------- The script will now add firewall rules and then disconnect any active remote sessions."
-Write-Host "---------- You should be able to reconnect using the new port (if disconnected)."
-Read-Host "Press Enter to continue..."
-
-# Firewall and services
-Write-Host "-- Adding firewall rules..."
-netsh advfirewall firewall add rule name="RDP Port $rdp_port" profile=any protocol=TCP action=allow dir=in localport=$rdp_port
-Write-Host "-- Stopping and starting terminal services..."
-Stop-Service -Name TermService -Force
-Start-Service -Name TermService
-
-# Done
-Write-Host "---------- Done"
+Write-Host "---------- Done ----------" -ForegroundColor Cyan
 Read-Host "Press Enter to exit..."
