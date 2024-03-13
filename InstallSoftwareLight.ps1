ef326ad844ac460e549ce9cfac9cdd7d941c480f
Roger <110936182+rogerbase@users.noreply.github.com>
2024-03-13 23:29:16 +0100

    Update InstallSoftwareLight.ps1

--- InstallSoftwareLight.ps1
+++ InstallSoftwareLight.ps1
@@ -14,29 +14,6 @@ choco install chromium -y -r
 ### Install 7-Zip
 Write-Host 'Installing 7-Zip...' -ForegroundColor Cyan
 choco install 7zip -y -r
-## Create handlers for archives
-$ArchiveHandlers = @("'HKLM\SOFTWARE\Classes\.7z' /ve /d '7-Zip.7z' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.7z' /ve /d '7z Archive' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.7z\DefaultIcon' /ve /d 'C:\Program Files\7-Zip\7z.dll,0' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.7z\shell' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.7z\shell\open' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.7z\shell\open\command' /ve /d 'C:\Program Files\7-Zip\7zFM.exe %1' /f",
-"'HKLM\SOFTWARE\Classes\.zip' /ve /d '7-Zip.zip' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.zip' /ve /d 'zip Archive' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.zip\DefaultIcon' /ve /d 'C:\Program Files\7-Zip\7z.dll,1' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.zip\shell' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.zip\shell\open' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.zip\shell\open\command' /ve /d 'C:\Program Files\7-Zip\7zFM.exe %1' /f",
-"'HKLM\SOFTWARE\Classes\.rar' /ve /d '7-Zip.rar' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.rar' /ve /d 'rar Archive' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.rar\DefaultIcon' /ve /d 'C:\Program Files\7-Zip\7z.dll,3' /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.rar\shell' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.rar\shell\open' /ve /f",
-"'HKLM\SOFTWARE\Classes\7-Zip.rar\shell\open\command' /ve /d 'C:\Program Files\7-Zip\7zFM.exe %1' /f")
-
-foreach ($ArchiveHandlersObject in $ArchiveHandlers) {
-  Invoke-Expression ("REG ADD " + $ArchiveHandlersObject) | Out-Null
-}
 
 ### Install Mem Reduct
 Write-Host 'Installing Mem Reduct...' -ForegroundColor Cyan
